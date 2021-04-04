e8932cc06400fae904d5b5e9b6482d6bc1974206
ahmedtariq11 <70329164+ahmedtariq11@users.noreply.github.com>
2021-04-04 22:53:04 +0500

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,14 @@
+//contact form 7 plugin through  gmail email send 
+define( 'SMTP_PORT',   '465' );                  // SMTP port number - likely to be 25, 465 or 587
+define( 'SMTP_SECURE', 'tls' );                 // Encryption system to use - ssl or tls
+define( 'SMTP_AUTH',    true );                 // Use SMTP authentication (true|false)
+define( 'SMTP_DEBUG',   0 );
+define( 'SMTP_USER',   'Hotel');    // Username to use for SMTP authentication
+define( 'SMTP_PASS',   '1ddrfz& ' );       // Password to use for SMTP authentication
+define( 'SMTP_HOST',   'smtp.reiddnas.pk');    // The hostname of the mail server
+define( 'SMTP_FROM',   'reinadds@gmail.com' ); // SMTP From email address
+define( 'SMTP_NAME',   'e.g Website Name' );    // SMTP From name
+define( 'SMTP_PORT',   '587' ,'487');                  // SMTP port number - likely to be 25, 465 or 587
+define( 'SMTP_SECURE', 'tls' );                 // Encryption system to use - ssl or tls
+define( 'SMTP_AUTH',    true );                 // Use SMTP authentication (true|false)
+define( 'SMTP_DEBUG',   0 );    
