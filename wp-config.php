7f13244461040d143a69f8926fe35200aaf303c0
AHMED ALI TARIQ <70329164+ahmedtariq11@users.noreply.github.com>
2021-04-05 11:00:40 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -3,9 +3,9 @@ define( 'SMTP_PORT',   '465' );                  // SMTP port number - likely to
 define( 'SMTP_SECURE', 'tls' );                 // Encryption system to use - ssl or tls
 define( 'SMTP_AUTH',    true );                 // Use SMTP authentication (true|false)
 define( 'SMTP_DEBUG',   0 );
-define( 'SMTP_USER',   'Hotel');    // Username to use for SMTP authentication
-define( 'SMTP_PASS',   '1ddrfz& ' );       // Password to use for SMTP authentication
-define( 'SMTP_HOST',   'smtp.reiddnas.pk');    // The hostname of the mail server
+define( 'SMTP_USER',   '');    // Username to use for SMTP authentication
+define( 'SMTP_PASS',   ' ' );       // Password to use for SMTP authentication
+define( 'SMTP_HOST',   '');    // The hostname of the mail server
 define( 'SMTP_FROM',   'reinadds@gmail.com' ); // SMTP From email address
 define( 'SMTP_NAME',   'e.g Website Name' );    // SMTP From name
 define( 'SMTP_PORT',   '587' ,'487');                  // SMTP port number - likely to be 25, 465 or 587
